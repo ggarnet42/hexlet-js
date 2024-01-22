c44bb946eff55db20bb1beb654dc4b239e66b29a
Diana Mukhamadieva <root@DESKTOP-ONU5PNH>
2024-01-22 13:29:13 +0300

    modified:   tests/capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
-import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
+import capitalize from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize(''), '');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
